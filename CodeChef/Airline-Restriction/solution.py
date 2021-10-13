965461c35118d51588c06b4a35a474a92ddcadb5
Harshit rathore <harshitrathore@gmail.com>
2021-10-13 14:16:47 +0530

    Add question

---
+++ CodeChef/Airline-Restriction/solution.py
@@ -0,0 +1,18 @@
+t = int(input())
+for i in range(t):
+    a,b,c,d,e = map(int,input().split())
+    n=0
+    if (a+b) <= d :
+        if c<=e :
+            n+=1
+    if (b+c) <=d :
+        if a<=e:
+            n+=1
+    if (a+c) <= d :
+        if b<=e:
+            n+=1
+    if n==0:
+        print("No")
+    else:
+        print("Yes")
+ 
